e13f433d7e7ae43e48afd3c741f2934722fa63b0
Benjamin Isong <beisongabc@gmail.com>
2020-08-10 18:12:39 +0100

    debug 3

--- resources/views/pages/about/team.blade.php
+++ resources/views/pages/about/team.blade.php
@@ -3,20 +3,21 @@
         <div class="team-item">
             <!-- Start single-item -->
             <div class="single-item wow fadeInUp">
-                <div class="img-holder" style="background-image: url('{{ url('img/user.png') }}'); background-repeat: no-repeat; background-position: center">
-                    <figure style="min-height: 268px"><a href="#"><img src="{{ url('https://smileplanetef.org/public/uploads/'.$volunteer['image']) }}" alt="Images"></a></figure>
+                <div class="img-holder" style="">
+                    <figure style="height: 230px; overflow: hidden">
+                        <a href="#"><img src="{{ url('https://smileplanetef.org/public/uploads/'.$volunteer['image']) }}" alt="Images"></a></figure>
                     <!-- Start overlay -->
                     <div class="overlay">
                         <div class="link-icon">
                             <ul class="link">
                                 <li>
-                                    <a href="{{ !empty($volunteer['fbook'])?"http://facebook.com/".str_replace(" ", "", str_replace("@", "", $volunteer['fbook'])):"#" }}"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="{{ !empty($volunteer['fbook'])?"http://facebook.com/".str_replace(" ", "", str_replace("@", "", $volunteer['fbook'])):"#" }}" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
                                 </li>
                                 <li>
-                                    <a href="{{ !empty($volunteer['igram'])?"http://instagram.com/".str_replace(" ", "", str_replace("@", "", $volunteer['igram'])):"#" }}"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                    <a href="{{ !empty($volunteer['igram'])?"http://instagram.com/".str_replace(" ", "", str_replace("@", "", $volunteer['igram'])):"#" }}" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
                                 </li>
                                 <li>
-                                    <a href="{{ !empty($volunteer['tweeta'])?"http://twitter.com/".str_replace(" ", "", str_replace("@", "", $volunteer['tweeta'])):"#" }}"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                    <a href="{{ !empty($volunteer['tweeta'])?"http://twitter.com/".str_replace(" ", "", str_replace("@", "", $volunteer['tweeta'])):"#" }}" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
                                 </li>
                             </ul>
                         </div>
