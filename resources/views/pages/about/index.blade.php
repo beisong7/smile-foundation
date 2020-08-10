2b180be354373c00cfb81b01664c43a35ce9927f
Benjamin Isong <beisongabc@gmail.com>
2020-08-10 17:51:51 +0100

    handle error

--- resources/views/pages/about/index.blade.php
+++ resources/views/pages/about/index.blade.php
@@ -120,19 +120,19 @@ $navbar['about'] = 'current';
                             <!-- Start single-item -->
                             <div class="single-item wow fadeInUp">
                                 <div class="img-holder" style="background-image: url('{{ url('img/user.png') }}'); background-repeat: no-repeat; background-position: center">
-                                    <figure style="min-height: 268px"><a href="#"><img src="{{ url('https://smileplanetef.org/uploads/'.$volunteer->image) }}" alt="Images"></a></figure>
+                                    <figure style="min-height: 268px"><a href="#"><img src="{{ url('https://smileplanetef.org/uploads/'.$volunteer['image']) }}" alt="Images"></a></figure>
                                     <!-- Start overlay -->
                                     <div class="overlay">
                                         <div class="link-icon">
                                             <ul class="link">
                                                 <li>
-                                                    <a href="{{ url($volunteer->fbook) }}"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                                    <a href="{{ url($volunteer['fbook']) }}"><i class="fa fa-facebook" aria-hidden="true"></i></a>
                                                 </li>
                                                 <li>
-                                                    <a href="{{ url($volunteer->igram) }}"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                                    <a href="{{ url($volunteer['igram']) }}"><i class="fa fa-instagram" aria-hidden="true"></i></a>
                                                 </li>
                                                 <li>
-                                                    <a href="{{ url($volunteer->tweeta) }}"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                                    <a href="{{ url($volunteer['tweeta']) }}"><i class="fa fa-twitter" aria-hidden="true"></i></a>
                                                 </li>
                                             </ul>
                                         </div>
@@ -140,7 +140,7 @@ $navbar['about'] = 'current';
                                     <!-- End overlay -->
                                 </div>
                                 <div class="text">
-                                    <h4><a href="#">{{ $volunteer->fname . " "  . $volunteer->lname}}</a></h4>
+                                    <h4><a href="#">{{ $volunteer['fname'] . " "  . $volunteer['lname']}}</a></h4>
                                     <p>volunteer</p>
                                 </div>
                             </div>
